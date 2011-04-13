bcd97858801a05531fe1ae8a1c8bd116a15f3a49
piha <pipiha@gmail.com>
2011-04-13 22:32:11 +0400

    catching json & file exceptions

--- simple-settings.js
+++ simple-settings.js
@@ -1,13 +1,15 @@
 var fs = require('fs');
 
 exports.init = function(defaultSettings, customSettings) {
-	if (defaultSettings.constructor == String) {
-		defaultSettings = JSON.parse(fs.readFileSync(defaultSettings, 'UTF-8'));
-	}
+	try {
+		if (defaultSettings.constructor == String) {
+			defaultSettings = JSON.parse(fs.readFileSync(defaultSettings, 'UTF-8'));
+		}
 
-	if (customSettings.constructor == String) {
-		customSettings = JSON.parse(fs.readFileSync(customSettings, 'UTF-8'));
-	}
+		if (customSettings.constructor == String) {
+			customSettings = JSON.parse(fs.readFileSync(customSettings, 'UTF-8'));
+		}
+	} catch(Error) {}
 	
 	if (customSettings instanceof Object) {
 		for (i in defaultSettings) {
